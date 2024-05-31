6f3758c38d2adb8bab835f904f702906fe0e38be
yusuken1121 <yusuken1121@gmail.com>
2024-05-31 09:05:10 -0700

    ✅ Creates Organisms

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,40 @@
+import { UserCardProps } from "../../../types/userCardProps";
+
+export const UserCard = ({
+  name,
+  email,
+  tell,
+  company,
+  website,
+}: UserCardProps) => {
+  return (
+    <div className="flex flex-col w-1/3 border-solid border-2 border-slate-500 rounded-lg m-3 bg-slate-400 p-3">
+      <div className=" flex w-full my-3 items-center justify-center">
+        <img
+          className="w-32 h-32 "
+          src="./src/assets/icon_Aru.jpg"
+          alt="profile"
+        />
+      </div>
+      <div className="bg-white p-2 rounded-lg">
+        <p className="font-bold">{name}</p>
+        <dl className="flex items-center">
+          <dt className="flex-1">E-mail</dt>
+          <dd className="dd-base flex-3">{email}</dd>
+        </dl>
+        <dl className="flex items-center">
+          <dt className="flex-1">Tell</dt>
+          <dd className="dd-base flex-3">{tell}</dd>
+        </dl>
+        <dl className="flex items-center">
+          <dt className="flex-1">Company</dt>
+          <dd className="dd-base flex-3">{company}</dd>
+        </dl>
+        <dl className="flex items-center">
+          <dt className="flex-1">Website</dt>
+          <dd className="dd-base flex-3">{website}</dd>
+        </dl>
+      </div>
+    </div>
+  );
+};
---
+++ src/types/userCardProps.ts
@@ -0,0 +1,7 @@
+export interface UserCardProps {
+  name: string;
+  email: string;
+  tell: number;
+  company: string;
+  website: string;
+}
